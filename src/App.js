a3c8029f818dc7d3dcd0931516e107c17532a7da
hmzunal <hamza.unalim@gmail.com>
2022-12-28 16:33:05 +0300

    toplu forn alanları ile ilgili örnek

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Iletisim from "./Iletisim";
+
+function App()
+{
+
+
+    return(
+       <Iletisim/>
+        )
+}
+
+export default App;
---
+++ src/Iletisim.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function Iletisim()
+{   
+    const [formalanlari,formAlanlariniGuncelle]=useState({})
+   
+    const topluGuncelle = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        formAlanlariniGuncelle(values => ({...values, [name]: value}))
+      }
+
+    const formGonder=(e)=>
+    {
+            e.preventDefault();
+            if(formalanlari.isim.length>10){
+                alert("isim 10 dan büyük olamaz")
+                return;
+            }
+            if(formalanlari.soyisim.length>10){
+                alert("soyisim 10 dan büyük olamaz")
+                return;
+            }
+            if(formalanlari.telefon.length>10){
+                alert("soyisim 10 dan büyük olamaz")
+                return;
+            }
+
+            alert("mesajınız gönderildi")
+    }
+
+    return(
+        <form onSubmit={formGonder} >
+            <div>
+            <p>Adınız:</p>
+            <input name="isim" type="text" onChange={topluGuncelle}></input>{formalanlari.isim?.length}
+            </div>
+            <div>
+            <p>Soyadınız:</p>
+            <input name="soyisim" type="text" onChange={topluGuncelle}></input> {formalanlari.soyisim?.length}
+            </div>
+            <div>
+            <p>Telefon:</p>
+            <input name="telefon" type="text" onChange={topluGuncelle}></input> {formalanlari.telefon?.length}
+            </div>
+            <div>
+            <p>Site:</p>
+            <input name="site" type="text" onChange={topluGuncelle}></input> {formalanlari.site?.length}
+            </div>
+
+        <button type="submit">Gönder</button>
+        </form>
+        
+        )
+}
+
+export default Iletisim;
